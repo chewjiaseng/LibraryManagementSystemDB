99ce23b26a7674bd365a86eff6a83c7b09bb1924
TUNG0922 <tungjinyou@gmail.com>
2024-01-14 18:27:10 +0800

    Changes & Edit Publisher and also the navbar designs and links

--- src/main/java/com/example/LibraryManagementSystem/service/CategoryService.java
+++ src/main/java/com/example/LibraryManagementSystem/service/CategoryService.java
@@ -5,6 +5,7 @@ import com.example.LibraryManagementSystem.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
 
 @Service
@@ -22,6 +23,11 @@ public class CategoryService {
     @Transactional
     public void deleteCategoryById(Integer categoryId) {
         categoryRepository.deleteById(categoryId);
+    }
+
+    public Category getCategoryById(Integer categoryId) {
+        Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
+        return optionalCategory.orElse(null);
     }
 }
 
